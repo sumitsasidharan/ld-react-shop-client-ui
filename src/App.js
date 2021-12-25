9d4b3d629bb925ab463d0bbbe86719504fde32d5
Sumit Sasidharan <s.sumit196@gmail.com>
2021-12-25 19:57:58 +0530

    added code for  responsiveness

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import Cart from './pages/Cart';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Product from './pages/Product';
-import ProductList from './pages/ProductList';
-import Register from './pages/Register';
 
 function App() {
    return (
       <div className="App">
-         <Cart />
+         <Home />
       </div>
    );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,11 @@ import {
    Email,
 } from '@material-ui/icons';
 import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
    display: flex;
+   ${mobile({ flexDirection: 'column' })};
 `;
 const Left = styled.div`
    flex: 1;
@@ -42,6 +44,7 @@ const SocialIcon = styled.div`
    margin-right: 20px;
 `;
 
+// ${mobile({ display: 'none' })}
 const Center = styled.div`
    flex: 1;
    padding: 20px;
@@ -65,6 +68,7 @@ const ListItem = styled.li`
 const Right = styled.div`
    flex: 1;
    padding: 20px;
+   ${mobile({ backgroundColor: '#eee' })};
 `;
 
 const ContactItem = styled.div`
